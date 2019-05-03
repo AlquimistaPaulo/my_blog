6506b98e83c5da93db1229daca83c05c3fd246dc
Kazuma <kazuma@example.com>
2019-05-03 11:58:12 +0900

    リファクタリング完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -24,17 +24,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to @post
   end
@@ -45,4 +42,8 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :category)
   end
+end
+
+def set_post
+  @post = Post.find(params[:id])
 end
