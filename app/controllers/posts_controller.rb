a758c2c168379a2996d65085250cc7eae1241fda
Kazuma <kazuma@example.com>
2019-05-03 14:21:23 +0900

    かみなり、スコープ追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @q = Post.order(created_at: :desc).ransack(params[:q])
-    @posts = @q.result(distinct: true)
-    @new_posts = Post.order(created_at: :desc).limit(5)
+    @posts = @q.result.page(params[:page]).per(2)
+
+    @new_posts = Post.find_newest_article
     @author = Author.first
   end
 
@@ -26,15 +28,20 @@ class PostsController < ApplicationController
 
   def edit
   end
-
+  
   def update
+    # updateメソッドを使って更新
     @post.update(post_params)
+    # 詳細画面にリダイレクト
     redirect_to @post
   end
 
   def destroy
+    # 対象データを1件取得する
+    # destroyアクションで消す
+    # 一覧ページにリダイレクトする
     @post.destroy
-    redirect_to @post
+    redirect_to posts_path
   end
 
   # プライベートメソッドでストロングパラメータを作成
@@ -43,8 +50,8 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :category)
   end
-end
 
-def set_post
-  @post = Post.find(params[:id])
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
